cce3d809def396251afb8c5188931d23ad343836
Gijs Nieuwenhuis <gijsnieuwenhuis@gmail.com>
2015-09-29 21:59:03 +0200

    Added check that is displayed when the user wants to leave the page

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,10 @@
 var React = require('react'),
     AppComponent = require('./component/appComponent');
 
+window.onbeforeunload = function () {
+    return 'Weet je zeker dat je wilt verversen? Eventuele geimporteerde code gaat dan verloren..';
+};
+
 React.render(
     <AppComponent />,
     document.getElementById('js-app-container')
